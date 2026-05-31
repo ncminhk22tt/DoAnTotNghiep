dec6b76ff7e8b66145413df00023d6083a9b3a75
HaiK22TT <nchai.k22tt@kontum.udn.vn>
2026-05-31 21:21:05 +0700

    update

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/lib/db";
 
 export async function GET() {
-  const [rows] = await db.query("SELECT 1");
+  const [rows] = await db.query("SELECT * FROM users LIMIT 5");
 
   return Response.json(rows);
 }
