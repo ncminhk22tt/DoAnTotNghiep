5dc646f706d2143b5f277be6388d2f09b30e0cd3
HaiK22TT <nchai.k22tt@kontum.udn.vn>
2026-06-04 20:33:07 +0700

    update1

--- src/app/(doctor)/doctor/layout.tsx
+++ src/app/(doctor)/doctor/layout.tsx
@@ -9,7 +9,7 @@ const navItems = [
   // { href: "/doctor/schedules/list", label: "Danh sách lịch" },
   { href: "/doctor/appointments", label: "Lịch hẹn" },
   { href: "/doctor/medical-records", label: "Hồ sơ bệnh án" },
-  { href: "/doctor/profile", label: "Hồ sơ cá nhân" },
+  { href: "/doctor/profile", label: "Hồ sơ bác sĩ" },
 ];
 
 export default function DoctorLayout({ children }: { children: React.ReactNode }) {
